7d98e2d7920fedecfc40c0e34207269fd32a8a1e
Benjamin Meixner <46966993+maixnor@users.noreply.github.com>
2024-03-20 15:16:48 +0100

    correct organization now

--- r/qm2/week2.R
+++ r/qm2/week2.R
@@ -1,97 +1,75 @@
 #' ---
-#' title: "Session 1: Probability "
+#' title: "Session 2: Probability-II "
 #' author: "QM-II "
 #' ---
 
-## Defining a vector with possible outcomes of a random variable
-out <- c( -1, 1, 2.4, 7.286 )
-
-## We also define a vector with probabilities for each of these outcomes
-p <- c( 1/6, 1/4, 1/3, 1/4)
+## library("extraDistr") might need to be installed first via: 
+#install.packages("extraDistr")
 
 
-## We can also define the distribution function
-F <- function(a) { 
-  sum((out <= a)*p )
-}
-x <- seq(-2, 10, 0.1)
-y <- sapply(x, F)
-plot(x, y, xlim=c(-2, 10), 'p')
-abline(v = out, col = 2)
+library("extraDistr")
+
+## ddunif is the density of the DISCRETE uniform
+ddunif(72, 1, 360)
+# probability for the outcome to be in 90 to 180 degrees
+print(sum(ddunif(91:180, 1, 360)))
+## Can also be calculated with the cumulatativ distribution function pdunif
+print(pdunif(180, 1, 360) - pdunif(90, 1, 360))  ## Why 90 here and 91 two lines above?
 
 
-## Let us calculate the expectation
-EX <- sum(out*p)
-cat("Expectation is: ", EX, "\n")
+## CONTINUOUS uniform uses a similar command:  dunif for the density and punif for
+## the cumulative distribution function. No special packages needed.
+print(punif(180, 0, 360) - punif(90, 0, 360))   ## Why here parameters 0 and 360 and not as above?
 
-
-## ... and the variance
-VX <- sum((out - EX)^2*p)
-cat("Expectation is: ", VX, "\n")
-
-
-## Or we bundle this in a single function that produces both
-EnVar <- function(out,p) {
-  EX <- sum(out*p)
-  VX <- sum((out - EX)^2*p)
-  return(c(EX, VX))
-}
-EnVar(out, p)
-
-## If we have another random variable we can produce the covariance
-outY <- c(2, 4, -1, 0)
-
-EY <- EnVar(outY, p)[1] 
-
-CovXY <- sum((out - EX)*(outY - EY)*p)  # note: this assumes that X and Y take on the defined values
-# simultaneously, that is, whenever X is -1, Y is 2, etc. - order matters!
-print(CovXY)
-
-
-#' For most of the distributions, the d/p-functions are available
-#' d	density function if continuous; OTHERWISE probability mass function
-#' p	(cumulative) distribution function
-
-# Example: Binomial Distribution with n = 10 trials and p = 0.5 success probability.
-# The following command produces the probabilities for the 11 possible outcomes.
-# The possible outcomes for a binomial distribution with n = 10 trials are the integers 
-# from 0 to 10.
-n <- 10
-p.suc <- 0.5
-
-out <- 0:n
-p <- dbinom(0:n, n, p.suc)
-
-cat("Outcomes: ", out, "\n\nProbabilities: ", p[0:4], "\n\n", p[5:11])
-
-
-## Formula from the lecture slides:
-cat("Expectation: ", n*p.suc, "\nVariance: ", n*p.suc*(1-p.suc))
-
-## And now with our routine
-cat("Expectation: ", EnVar(out, p)[1], "\nVariance: ", EnVar(out, p)[2])
-
-
-#' Recall: function choose(k, n) calculates the number of sets with
-#' n elements than can be chosen from a set with k elements, that is the binomial coefficient
-choose(3, 5)  ## Why is the result zero?
-choose(5, 3)
-
-
-#Please check the following distributions and repeat the above exercises:
-#Poisson Ditribution: d/p pois() 
-lambda <- 1
-N <- 1000  # cutoff point. You need this as the possible outcomes range from 0 to infinity!
-out <- 0:N
-p <- dpois(0:N, lambda)
-
-# ... now its your turn
-
-## Formula from the lecture slides:
-cat("Expectation: ",NULL,"\nVariance: ",NULL)
+## We can plot the density function
+f <- function(x) dunif(x, 0, 360)
+plot(f, xlim = c(0, 360), col = 2)
 
 
 
+## The normal distribution uses the:  dnorm, pnorm  commands 
+# the commands with default options provide values for standard normal distribution
+plot(pnorm, xlim = c(-3, 3), ylim = c(0, 1), main = "Cumulative (standard) normal distribution 
+function (black) and density function (red)")
+par(new = TRUE) # makes sure that the next command only adds to the plot instead of creating a new one
+plot(dnorm, xlim = c(-3, 3), ylim = c(0, 1), col=2)
+
+##We also have the command: qnorm 
+# qnorm does the opposite of pnorm: given an area under the normal density,
+# i.e. given a probability, it finds the boundary value that determines this area.
+z_80 <- qnorm(0.80, mean = 50, sd = 3)
+
+# We have the following interpretation: 80% of the values in a population that is
+# normally distributed with mean 50 and standard deviation 3 will lie below z_80.
+
+## The log-normal distribution uses the:  dlnorm, plnorm  commands
+plot(plnorm, xlim = c(-3, 3), ylim=c(0, 1), main = "Cumulative log-normal distribution 
+function (black) and density function (red)")
+par(new = TRUE)
+plot(dlnorm, xlim = c(-3, 3), ylim = c(0, 1), col = 2)
 
 
+
+## One has the functional relation of the cumulative distribution functions:
+## pnorm(x) = plnorm(exp(x))  for any x
+x = seq(-3, 3, by = 0.1)
+z = pnorm(x) - plnorm(exp(x))
+round(max(abs(z)), digits = 10)
+
+## The density is the derivative of the distribution function. Taking the derivative
+## of the functional equation yields:
+## dnorm(x) = dlnorm(exp(x)) * exp(x)
+
+
+## Exponential distribution is via the commands dexp, pexp
+# Be careful about the parameter specification of R: e.g., dexp(x,rate = lambda) 
+# gives the value of the density of an exponentially distributed random variable with 
+# mean (expected) value 1/lambda.  
+
+plot(pexp, xlim = c(-3, 3), ylim = c(0, 1), main = "Cumulative exponential
+distribution function (black) and density function (red)")
+par(new = TRUE)
+plot(dexp, xlim = c(-3, 3), ylim = c(0, 1), col = 2)
+# Warning: The plotted image reveals a numerical instability of R's plot command. 
+# The red line should start to move up from zero NOT before
 
