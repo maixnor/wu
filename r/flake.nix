6a2e3c0eb0d29b9aabe97bea572192029a2abad2
maixnor <46966993+maixnor@users.noreply.github.com>
2024-12-01 19:13:49 +0100

    bierbasis snapshot

--- r/flake.nix
+++ r/flake.nix
@@ -10,27 +10,7 @@
         pkgs = import nixpkgs { inherit system; };
     in {
         packages = {
-            default = pkgs.mkShell { # also works as dev shell
-                buildInputs = with pkgs; [
-                    R
-                    rPackages.rmarkdown
-                    rPackages.extraDistr
-                    rPackages.dplyr
-                    rPackages.ggplot2
-                    rPackages.psych
-                    rPackages.corrplot
-                    rPackages.Hmisc
-                    rPackages.apaTables
-                    rPackages.nFactors
-                    rPackages.qgraph
-                    tectonic
-                    pandoc
-                    inotify-tools
-                    bat
-                ];
-            };
-
-            markdown = pkgs.stdenv.mkDerivation {
+            default = pkgs.stdenv.mkDerivation {
                 name = "build-rmds";
                 src = ./.;
                 buildInputs = with pkgs; [ 
@@ -43,19 +23,29 @@
                     rPackages.corrplot
                     rPackages.Hmisc
                     rPackages.apaTables
-                    tectonic 
+                    rPackages.nFactors
+                    rPackages.qgraph
+                    rPackages.xts
+                    rPackages.lubridate
+                    tectonic
                     pandoc 
+                    inotify-tools
+                    bat
+                    git
                 ];
 
                 buildPhase = ''
-                    for file in *.Rmd; do
+                    for file in $(git diff --name-only @{20} | grep -E '\.R$|\.Rmd$'); do
                         Rscript -e "rmarkdown::render('$file')"
                     done
                 '';
 
                 installPhase = ''
                     mkdir -p $out
-                    cp *.pdf $out
+                    for file in $(find -name '*.pdf'); do
+                        cp $file $out
+                    done
+                    echo "Done"
                 '';
             };
         };
