2942d07b5f53dbc834248c3ceda0c16ecb72c246
Benjamin Meixner <46966993+maixnor@users.noreply.github.com>
2025-01-28 12:28:00 +0100

    tutoring II

--- r/qm1/tutoring.R
+++ r/qm1/tutoring.R
@@ -1,5 +1,14 @@
 
-# A function that mirrors a matrix around the x-axis
+# nested for loop
+for (i in 1:3) {
+    sum <- 0
+    for (j in 1:5) {
+        sum <- sum + a[i,j]
+    }
+    print(sum)
+}
+
+# tell me what does does?
 mirror_matrix_x <- function(mat) {
     mirrored_mat <- matrix(nrow = nrow(mat), ncol = ncol(mat))
     for (i in 1:nrow(mat)) {
@@ -9,11 +18,15 @@ mirror_matrix_x <- function(mat) {
 }
 
 # a function that calculates the rieman sum of a vector of values
-riemann_sum <- function(values, x) {
+# word cloud:
+# 0, 1, 2
+# values, sum, i
+# length, dx
+
+riemann_sum <- function(values, dx) {
     sum <- 0
     for (i in 2:length(values)) {
-        dx <- x[i] - x[i - 1]
-        sum <- sum + values[i - 1] * dx
+        sum <- sum + values[i] * dx
     }
     return(sum)
 }
@@ -22,34 +35,29 @@ riemann_sum <- function(values, x) {
 det_and_inverse <- function(mat) {
     n <- nrow(mat)
     if (n != ncol(mat)) {
-        stop("Matrix must be square")
+        stop("Matrix is not square!")
     }
     
     if (n == 2) {
         det <- mat[1,1] * mat[2,2] - mat[1,2] * mat[2,1]
         if (det == 0) {
-            stop("Matrix is singular and cannot be inverted")
+            stop("Cannot inverse when determinant is 0!")
         }
         inv <- (1 / det) * matrix(c(mat[2,2], -mat[1,2], -mat[2,1], mat[1,1]), nrow = 2)
     } else {
-        stop("Only 2x2 are supported")
+        stop("This is not a 2x2 matrix!")
     }
     inv    
 }
 
-# a function that wraps constrOptim for a 2-variable optimization problem
-# params: profit function, min x, min y, max x+y
 optimize_profit <- function(profit_func, min_x, min_y, max_sum) {
-    # Initial guess
-    initial_guess <- c((min_x + max_sum) / 2, (min_y + max_sum) / 2)
+    initial_guess <- c(min_x + 0.1, min_y + 0.1)
     
-    # Constraints: x >= min_x, y >= min_y, x + y <= max_sum
     ui <- rbind(c(1, 0), c(0, 1), c(1, 1), c(-1, -1))
     ci <- c(min_x, min_y, max_sum, -max_sum)
     
-    # Perform constrained optimization with fnscale = -1
-    result <- constrOptim(initial_guess, profit_func, NULL, ui, ci, control = list(fnscale = -1))
+    result <- constrOptim(initial_guess, -profit_func, grad=NULL, ui=ui, ci=ci, control = list(fnscale = -1))
     
-    # Return the optimal values of x and y
     return(result$par)
 }
+optimize_profit(\(x, y) {x^2 + y^2}, 0, 0, 10)
