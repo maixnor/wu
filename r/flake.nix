f27328a99f5b38d51010d364f1b0ab1341f6e3d3
Benjamin Meixner <46966993+maixnor@users.noreply.github.com>
2025-03-20 09:46:03 +0100

    fix flake.nix

--- r/flake.nix
+++ r/flake.nix
@@ -6,34 +6,30 @@
         flake-utils.url = "github:numtide/flake-utils";
     };
 
-    outputs = { nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: let
+    outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: let
         pkgs = import nixpkgs { inherit system; };
     in {
         devShells.default = pkgs.stdenv.mkDerivation {
-          buildInputs = with pkgs; [ 
-            R 
-            rPackages.rmarkdown 
-            rPackages.extraDistr
-            rPackages.dplyr
-            rPackages.ggplot2
-            rPackages.ggthemes
-            rPackages.psych
-            rPackages.corrplot
-            rPackages.Hmisc
-            rPackages.apaTables
-            rPackages.nFactors
-            rPackages.qgraph
-            rPackages.xts
-            rPackages.lubridate
-            rPackages.tidyverse
-            rPackages.viridisLite
-            rPackages.Benchmarking
-            tectonic
-            pandoc 
-            inotify-tools
-            bat
-            git
-          ];
+            name = "build-rmds";
+            src = ./.;
+            buildInputs = with pkgs; [ 
+                rPackages.rmarkdown 
+                rPackages.extraDistr
+                rPackages.dplyr
+                rPackages.ggplot2
+                rPackages.ggthemes
+                rPackages.psych
+                rPackages.corrplot
+                rPackages.Hmisc
+                rPackages.apaTables
+                rPackages.nFactors
+                rPackages.qgraph
+                rPackages.xts
+                rPackages.lubridate
+                rPackages.tidyverse
+                rPackages.viridisLite
+                rPackages.Benchmarking
+            ];
         };
     });
 }
